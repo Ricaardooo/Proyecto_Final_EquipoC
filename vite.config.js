a43026a9868c196cc21ec280d28ec88951edd49a
Ricaardooo <guillenricardo.1704@gmail.com>
2026-04-11 22:03:19 -0600

    Corrección de errores en .github
    Corrección de errores de despliegue

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,12 @@ import { defineConfig } from "vite";
 import handlebars from "vite-plugin-handlebars";
 
 export default defineConfig({
+  base: '/Proyecto_Final_EquipoC-main/',
+
   plugins: [
     handlebars({
       partialDirectory: "partials",
     }),
   ],
 });
+
