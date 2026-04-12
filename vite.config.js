1892821f6b44b200d30c785e2fd848d88e0ab892
Zoraluz <paprikaa816@gmail.com>
2026-04-12 02:24:21 -0600

    Prueba pages 8
    Cambios al vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,41 @@
-import { defineConfig } from "vite";
-import handlebars from "vite-plugin-handlebars";
+import { defineConfig } from 'vite';
+import handlebars from 'vite-plugin-handlebars';
+import purgecss from 'vite-plugin-purgecss';
 
-export default defineConfig({
-  base: '/Proyecto_Final_EquipoC/',
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/Proyecto_Final_EquipoC/' : '/',
+
+  build: {
+    cssMinify: true,
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        acerca: 'acerca.html',
+        blog: 'blog.html',
+        catalogo: 'catalogo.html',
+        contacto: 'contacto.html',
+        faq: 'faq.html',
+        galeria: 'galeria.html',
+        login: 'login.html',
+        ofertas: 'ofertas.html',
+        soporte: 'soporte.html',
+      },
+    },
+  },
+
+  server: {
+    port: 5173,
+  },
 
   plugins: [
     handlebars({
-      partialDirectory: "partials",
+      partialDirectory: './partials',
+    }),
+    purgecss({
+      content: [
+        './*.html',
+        './javascript/**/*.js',
+      ],
     }),
   ],
-});
-
+}));
